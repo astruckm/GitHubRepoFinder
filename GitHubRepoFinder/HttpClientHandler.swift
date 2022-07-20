6fb11787bf8906e2c6caf2e1de17b0342bde84a8
Andrew Struck-Marcell <astruckm@yahoo.com>
2022-07-20 23:21:23 +0900

    Add networking protocol, HttpClientHandler

---
+++ GitHubRepoFinder/HttpClientHandler.swift
@@ -0,0 +1,30 @@
+//
+//  HttpClientHandler.swift
+//  GitHubRepoFinder
+//
+//  Created by Andrew Struck-Marcell on 7/14/22.
+//
+
+import Foundation
+
+protocol HttpClientHandler: AnyObject {
+    func handleDataTaskErrors(data: Data?, response: URLResponse?, error: Error?) -> Result<Data, NetworkingError>
+}
+
+extension HttpClientHandler {
+    func handleDataTaskErrors(data: Data?, response: URLResponse?, error: Error?) -> Result<Data, NetworkingError> {
+        if let error = error {
+            return .failure(NetworkingError.apiError(error))
+        }
+        guard let httpResponse = response as? HTTPURLResponse else {
+            return .failure(NetworkingError.wrongRequestType(response))
+        }
+        guard 200 ..< 300 ~= httpResponse.statusCode else {
+            return .failure(NetworkingError.badURLResponse(httpResponse.statusCode))
+        }
+        guard let data = data, !data.isEmpty else {
+            return .failure(NetworkingError.noData)
+        }
+        return .success(data)
+    }
+}
