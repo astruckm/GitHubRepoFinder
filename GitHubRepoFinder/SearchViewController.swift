bb8b53b0810007ba297c235c45f8bb7165c4461a
Andrew Struck-Marcell <astruckm@yahoo.com>
2022-07-21 22:18:52 +0900

    Setup table node with dummy code

--- GitHubRepoFinder/SearchViewController.swift
+++ GitHubRepoFinder/SearchViewController.swift
@@ -10,20 +10,29 @@ import AsyncDisplayKit
 import AuthenticationServices
 
 class SearchViewController: ASDKViewController<ASDisplayNode> {
+    let tableNode: ASTableNode
+    
     let viewModel: SearchViewModel
     let client = GitHubApiClient()
     let oauthClient = GitHubOAuthClient()
     
     override init() {
-        let node = ASDisplayNode()
         viewModel = SearchViewModel()
+
+        let node = ASDisplayNode()
+        let tableNode = ASTableNode(style: .plain)
+        self.tableNode = tableNode
         super.init(node: node)
         
-        // TODO: can set data source, delegate here
+        tableNode.dataSource = self
+        tableNode.delegate = self
     }
     
     required init?(coder: NSCoder) {
         viewModel = SearchViewModel()
+        
+        let tableNode = ASTableNode(style: .plain)
+        self.tableNode = tableNode
         super.init(coder: coder)
     }
     
@@ -31,6 +40,8 @@ class SearchViewController: ASDKViewController<ASDisplayNode> {
         super.viewDidLoad()
         
         node.backgroundColor = .systemBackground
+        node.addSubnode(tableNode)
+        tableNode.frame = node.frame
         
         navigationController?.navigationBar.backgroundColor = .systemGray6
 //        navigationController?.navigationBar.shadowImage = UIColor.darkGray.image(CGSize(width: view.frame.width, height: 1))
@@ -58,10 +69,32 @@ class SearchViewController: ASDKViewController<ASDisplayNode> {
         
 }
 
-
 extension SearchViewController: ASWebAuthenticationPresentationContextProviding {
     func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
         return view.window ?? UIWindow()
+    }
+}
+
+extension SearchViewController: ASTableDataSource {
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        // TODO: grab repo object here
+        return {
+            let node = ASCellNode()
+            node.backgroundColor = .blue
+            return node
+        }
+    }
+}
+
+extension SearchViewController: ASTableDelegate {
+    func tableNode(_ tableNode: ASTableNode, constrainedSizeForRowAt indexPath: IndexPath) -> ASSizeRange {
+        let min = CGSize(width: self.view.bounds.width, height: 100)
+        let max = CGSize(width: self.view.bounds.width, height: 200)
+        return ASSizeRange(min: min, max: max)
     }
 }
 
