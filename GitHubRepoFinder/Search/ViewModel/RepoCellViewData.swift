8e4aeb10c51e88f5c1103e1572e01986b8a4b692
Andrew Struck-Marcell <astruckm@yahoo.com>
2022-08-13 13:40:59 +0900

    Search view models in new folder

---
+++ GitHubRepoFinder/Search/ViewModel/RepoCellViewData.swift
@@ -0,0 +1,26 @@
+//
+//  RepoCellViewData.swift
+//  GitHubRepoFinder
+//
+//  Created by Andrew Struck-Marcell on 8/13/22.
+//
+
+import Foundation
+
+class RepoCellViewData {
+    let title: String
+    let description: String
+    let language: String
+    let numStars: Int
+    var readMeFullHTML: String?
+    var imageURL: URL?
+        
+    init(title: String, description: String, language: String, numStars: Int, repoFullHTML: String? = nil, imageURL: URL? = nil) {
+        self.title = title
+        self.description = description
+        self.language = language
+        self.numStars = numStars
+        self.readMeFullHTML = repoFullHTML
+        self.imageURL = imageURL
+    }
+}
--- GitHubRepoFinder/Search/ViewModel/SearchViewModel.swift
+++ GitHubRepoFinder/Search/ViewModel/SearchViewModel.swift
@@ -7,24 +7,6 @@
 
 import Foundation
 
-class RepoCellViewData {
-    let title: String
-    let description: String
-    let language: String
-    let numStars: Int
-    var readMeFullHTML: String?
-    var imageURL: URL?
-        
-    init(title: String, description: String, language: String, numStars: Int, repoFullHTML: String? = nil, imageURL: URL? = nil) {
-        self.title = title
-        self.description = description
-        self.language = language
-        self.numStars = numStars
-        self.readMeFullHTML = repoFullHTML
-        self.imageURL = imageURL
-    }
-}
-
 class SearchViewModel {
     let client = GitHubApiClient()
     let oauthClient = GitHubOAuthClient()
